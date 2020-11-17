cbaadbedb452784bf757a8d34495e8a32c5162ac
stephanogiuseppe <stephano.grp@gmail.com>
2020-11-17 00:55:35 -0300

    chore: add Produto (model, route, import, etc)

---
+++ src/Handler/Produto.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Produto where
+
+import Import
+
+getProdutoR :: Handler Html
+getProdutoR = undefined
+
+postProdutoR :: Handler Html
+postProdutoR = undefined
