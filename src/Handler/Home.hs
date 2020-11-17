bd1aabf9ac9b117db389e8907e944a5a52a8daea
stephanogiuseppe <stephano.grp@gmail.com>
2020-11-17 01:10:39 -0300

    feat: sample layout

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Home where
 
 import Import
@@ -10,4 +11,17 @@ import Import
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_bootstrap_css)
+        [whamlet|
+            <h1>
+                Sistema de Produtos
+
+            <img src=@{StaticR img_big_red_pot_with_glass_jpg}>
+
+            <ul>
+                <li>
+                    <a href=@{ProdutoR}>
+                        Cadastro de Produtos
+        |]
