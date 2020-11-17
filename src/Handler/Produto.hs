121b34b8370572f14b88493ebf364904faec88b6
stephanogiuseppe <stephano.grp@gmail.com>
2020-11-17 08:14:42 -0300

    feature(produto): add produto page [WIP]

--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -3,12 +3,29 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Produto where
 
 import Import
 
+-- gera html das caixax de form text
+formProduto :: Form Produto
+formProduto = renderDivs $ Produto
+    <$> areq textField (FieldSettings "Nome: "
+                            (Just "nome do produto") 
+                            (Just "hs1") Nothing
+                            [("class", "classe1")]
+                        ) Nothing
+    <*> areq doubleField "Preço: " Nothing
+
 getProdutoR :: Handler Html
-getProdutoR = undefined
+getProdutoR = do
+    (widget, _) <- generateFormPost formProduto
+    defaultLayout [whamlet|
+        <form action=@{ProdutoR} method=post>
+            ^{widget}
+            <input type="submit" value="Cadastrar">
+    |]
 
 postProdutoR :: Handler Html
 postProdutoR = undefined
