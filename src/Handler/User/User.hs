4bb33e9daa20a498021e6d0907ab1fb54a668b31
stephanogiuseppe <stephano.grp@gmail.com>
2020-11-23 20:58:52 -0300

    feat: user registration validation

--- src/Handler/User/User.hs
+++ src/Handler/User/User.hs
@@ -15,7 +15,7 @@ formUsu = renderBootstrap2 $ (,)
         <$> areq textField "Nome: " Nothing
         <*> areq emailField "E-mail: " Nothing
         <*> areq passwordField "Senha: " Nothing)
-    <*> areq passwordField "Digite Novamente: " Nothing
+    <*> areq passwordField "Digite a senha novamente: " Nothing
 
 getUserR :: Handler Html
 getUserR = do 
@@ -36,19 +36,43 @@ postUserR = do
     case result of 
         FormSuccess (user, userVerlidation) -> do 
             if (userSenha user == userVerlidation) then do 
-                _ <- runDB $ insert user 
-                setSession "_EMAIL" (userNome user)
-                setMessage [shamlet|
-                    <div>
-                        Usuário incluído
-                |]
-                redirect UserR
+                userDB <- runDB $ getBy (UniqueEmail (userEmail user))
+                case userDB of 
+                    Nothing -> do
+                        _ <- runDB $ insert user 
+                        setSession "_EMAIL" (userNome user)
+                        setMessage [shamlet|
+                            <div class="alert alert-primary" role="alert">
+                                Gol do Marinho.
+                                <br>
+                                <b>
+                                    Usuário criado com sucesso!
+                        |]
+                        redirect EntrarR
+                    Just (Entity _ _) -> do 
+                        setMessage [shamlet|
+                            <div class="alert alert-danger" role="alert">
+                                Lucas Braga entrou em campo!
+                                <br>
+                                <b>
+                                    E-mail já cadastrado
+                        |]
+                        redirect EntrarR 
             else do 
                 setMessage [shamlet|
-                    <div>
-                        SENHA E VERIFICACAO N CONFEREM
+                    <div class="alert alert-danger" role="alert">
+                        Lucas Braga foi relacionado!
+                        <br>
+                        <b>
+                            Senha e verificação não conferem
                 |]
                 redirect UserR
+        FormFailure (errMsg) -> do
+            setMessage [shamlet|
+                <div class="alert alert-danger" role="alert">
+                    Falha: #{show $ errMsg}
+            |]
+            redirect HomeR
         _ -> redirect HomeR
 
 
